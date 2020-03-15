4cc193d41ca51c98c4977d216d46df15c976241e
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-03-15 23:47:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+katz_deli = ["Cha", "Ricardo"]
+
+
+
+
+def take_a_number(katz_deli, string)
+katz_deli << string
+puts "Welcome #{string}. You are number #{katz_deli.length} in line."
+
+end
+
+
+
+
+def line(katz_deli)
+puts "The current line is: "
+    katz_deli.map.with_index {|name,index|  puts "#{index+1}. #{name}"}
+
+end
+
+
+
+def now_serving(katz_deli)
+puts "Now Serving: #{katz_deli[0]}"
+katz_deli.shift
+
+end# Write your code here.
