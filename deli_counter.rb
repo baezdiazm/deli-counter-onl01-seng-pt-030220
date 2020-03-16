6fe9bb3381041a4f49fb46914b5c8e1de7ea17d2
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-03-16 00:25:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ def line(katz_deli)
   if katz_deli.length < 1
     puts "The line is currently empty."
   else
-puts "The current line is: " + "#{katz_deli.each_with_index {|name,index| puts " #{index+1}. #{name} " }}"
+    print "The current line is: #{katz_deli.each_with_index {|name,index| print " #{index+1}. #{name} " }}"
   end
 end
 
